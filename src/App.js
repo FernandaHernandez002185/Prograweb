4fb973a471ee7c5398ccbcd7c8810dcae12cf82f
FernandaHernandez002185 <101229287+FernandaHernandez002185@users.noreply.github.com>
2022-11-27 21:55:28 -0600

    Footer pedorro

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import * as ReactBootStrap from "react-bootstrap";
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="page-container">
+        <div className='content-wrap'>
+            <Footer />
+        </div>
+    </div>
+
+  );
+}
+
+export default App;
